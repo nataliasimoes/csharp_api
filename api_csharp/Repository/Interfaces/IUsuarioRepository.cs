f165473a6b3026ea3795c01db7db56c43a4f9018
Natália Simões <natalia.simoes.dev@gmail.com>
2025-05-24 20:49:27 -0300

    Created user repository interface

---
+++ api_csharp/Repository/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using api_csharp.Models;
+
+namespace api_csharp.Repository.Interfaces;
+
+public interface IUsuarioRepository
+{
+    // O Task cria uma função assincrona
+    Task<List<UsuarioModel>> GetAllUsers();
+    Task<UsuarioModel> GetById(int id);
+    Task<UsuarioModel> AddUser(UsuarioModel usuario);
+    Task<UsuarioModel> UpdateUser(UsuarioModel usuario, int id);
+    Task<bool> Delete(int id);
+}
