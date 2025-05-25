1ba62332ba7d0f6cf45da1d8bb5d288b6d42000e
Natália Simões <natalia.simoes.dev@gmail.com>
2025-05-24 21:05:39 -0300

    Added StatusTarefa enum

---
+++ api_csharp/Enums/StatusTarefaEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace CadastroDeContatos.Enums;
+
+public enum StatusTarefaEnum
+{
+    [Description("A fazer")]
+    ToDo = 1,
+
+    [Description("Em andamento")]
+    InProgress = 2,
+
+    [Description("Concluído")]
+    Completed = 3
+}
--- api_csharp/Models/TarefaModel.cs
+++ api_csharp/Models/TarefaModel.cs
@@ -1,9 +1,11 @@
-﻿namespace api_csharp.Models;
+﻿using CadastroDeContatos.Enums;
+
+namespace api_csharp.Models;
 
 public class TarefaModel
 {
     public int Id { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
-    public int Status { get; set; }
+    public StatusTarefaEnum Status { get; set; }
 }
