cb1f998b6a01fc76f0b74d757630472afebfe76a
Natália Simões <natalia.simoes.dev@gmail.com>
2025-06-13 21:08:10 -0300

    Adds user link to task

--- api_csharp/Models/TarefaModel.cs
+++ api_csharp/Models/TarefaModel.cs
@@ -8,4 +8,6 @@ public class TarefaModel
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
     public StatusTarefaEnum Status { get; set; }
+    public int? UsuarioId { get; set; } // pode ser nula porque pode ser criada sem usuário e depois ser escolhida por ele
+    public virtual UsuarioModel? Usuario { get; set; }
 }
