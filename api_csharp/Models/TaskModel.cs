2d17508c7acd1c22dd02af796a6c268835eeccd0
Natalia Simões <francisca.araujo@imobme.com>
2025-07-04 23:04:26 -0300

    Fix models columns names

--- api_csharp/Models/TaskModel.cs
+++ api_csharp/Models/TaskModel.cs
@@ -1,16 +1,23 @@
 ﻿using api_csharp.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api_csharp.Models;
 
 public class TaskModel
 {
     public int Id { get; set; }
+    [Column("Nome")]
     public string? Name { get; set; }
+    [Column("Descricao")]
     public string? Description { get; set; }
     public StatusTaskEnum Status { get; set; }
+    [Column("UsuarioId")]
     public int? UserId { get; set; } // pode ser nula porque pode ser criada sem usuário e depois ser escolhida por ele
+    [Column("DataUltimaAlteracao")]
     public DateTime UpdatedAt { get; set; }
+    [Column("DataPrazo")]
     public DateTime? DueDate { get; set; }
+    [Column("DataConclusao")]
     public DateTime? CompletedAt { get; set; }
 
     public virtual UserModel? User { get; set; }
--- api_csharp/Models/UserModel.cs
+++ api_csharp/Models/UserModel.cs
@@ -1,10 +1,15 @@
-﻿namespace api_csharp.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_csharp.Models;
 
 public class UserModel
 {
     public int Id { get; set; }
+    [Column("Nome")]
     public string Name { get; set; }
     public string Email { get; set; }
+    [Column("Senha")]
     public string Password { get; set; }
+    [Column("DataUltimaAlteracao")]
     public DateTime UpdatedAt { get; set; }
 }
